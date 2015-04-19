63a142941a3e27a34079920e55456a994f1f5154
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-19 11:31:51 -0700

    Adding tests for rules

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -6,4 +6,19 @@ describe Varify::Base do
     foo = Varify::Base.varify(:foo,{foo:'bar'})
     expect(foo).to eq('bar')
   end
+
+  it 'raises on unrecognized rule' do
+    expect {
+      Varify::Base.varify(:foo,{foo:'bar'},{bad_rule:''})  
+    }.to raise_error "Rule ':bad_rule' is not defined"
+  end
+
+  it 'gets a message on failed verification' do
+    logs = []
+    Varify::Base.callback { |error| logs << error }
+
+    Varify::Base.varify(:foo,{},{required:true})  
+
+    expect(logs).to contain_exactly({message:"Foo (:foo) is required",key: :foo, rule: :required})
+  end
 end
